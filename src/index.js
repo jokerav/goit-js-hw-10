f120f74bf7b99c48a51c0da4685e000a5b7ed10f
jokerav <50701756+jokerav@users.noreply.github.com>
2022-02-21 22:18:57 +0200

    Update index.js

--- src/index.js
+++ src/index.js
@@ -4,9 +4,17 @@ const DEBOUNCE_DELAY = 300;
 
 const inputSearchBox = document.querySelector('#search-box');
 
-function fetchCountries(name) {
+function fetchCountries(names) {
   const BASIC_URL = 'https://restcountries.com/v3.1/';
-  return fetch(`${BASIC_URL}name/${name}`).then(response => response.json());
+  return fetch(`${BASIC_URL}name/${names}`)
+    .then(response => {
+      if (!response.ok) {
+        throw new Error(response.message);
+      }
+      return response.json();
+    })
+    .then(states => states.map(state => state.name.common));
 }
 
-console.log(fetchCountries('peru'));
+console.log(fetchCountries('japan, peru'));
+// console.log(ArrayOfStates);
